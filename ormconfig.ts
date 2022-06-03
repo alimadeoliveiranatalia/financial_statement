b5101df9d87ea4f24bfbddf362d0c8232cdc3bdd
Natalia <alimadeoliveiranatalia@gmail.com>
2022-06-02 22:14:16 -0300

    Camuflando credenciais de database

---
+++ ormconfig.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+export default
+{
+  "username": process.env.username,
+  "password": process.env.password,
+
+  "name": "default",
+  "type": "postgres",
+  "host": process.env.host,
+  "port": process.env.port,
+  "database": process.env.database,
+  "entities": [
+    "./src/modules/**/entities/*.ts"
+  ],
+  "migrations": [
+    "./src/database/migrations/*.ts"
+  ],
+  "cli": {
+    "migrationsDir": "./src/database/migrations"
+  }
+}
